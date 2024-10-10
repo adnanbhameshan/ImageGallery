ecf26f92501fef39d3d7d90077f459dc53d4bcb3
Adnan Ahmed Bhameshan <121448498+adnanbhameshan@users.noreply.github.com>
2024-10-11 00:41:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const galleryItems = document.querySelectorAll('.gallery-item');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const filter = button.getAttribute('data-filter');
+
+            galleryItems.forEach(item => {
+                if (filter === 'all' || item.getAttribute('data-category') === filter) {
+                    item.style.display = 'block';
+                } else {
+                    item.style.display = 'none';
+                }
+            });
+        });
+    });
+    document.querySelector('.filter-btn[data-filter="all"]').click();
+});
